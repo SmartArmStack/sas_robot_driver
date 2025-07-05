026d164d5bbb8ad9d047da0656be93cb325f39d6
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-05 20:16:08 +0100

    [ros_composer] Fixing call for declare_parameter to add default value.

--- src/sas_robot_driver_ros_composer_node.cpp
+++ src/sas_robot_driver_ros_composer_node.cpp
@@ -70,7 +70,7 @@ int main(int argc, char** argv)
             get_ros_parameter(node,"vrep_dynamically_enabled",robot_driver_ros_composer_configuration.coppeliasim_dynamically_enabled_);
 
             //25.7.5 Optional parameters to not break compatibility
-            node->declare_parameter("vrep_timeout");
+            node->declare_parameter("vrep_timeout", 1000);
             node->get_parameter("vrep_timeout",robot_driver_ros_composer_configuration.coppeliasim_timeout)
         }
         else
