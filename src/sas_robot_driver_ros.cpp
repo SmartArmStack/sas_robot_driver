b56df18398c9aa88463c0863d39769b11ec31154
Juancho <juanjqogm@gmail.com>
2026-07-31 17:34:52 +0100

    Improved the exception handling when the watchdog triggers an emergency stop

--- src/sas_robot_driver_ros.cpp
+++ src/sas_robot_driver_ros.cpp
@@ -171,7 +171,13 @@ int RobotDriverROS::control_loop()
 
                 // Any exception from the watchdog thread control loop will be rethrown by check_for_watchdog_exceptions(), and
                 // consequently the main control loop must stop.
-                robot_driver_->check_for_watchdog_exceptions();
+                try {
+                    robot_driver_->check_for_watchdog_exceptions();
+                } catch (const std::exception& e) {
+                    RCLCPP_INFO_STREAM_ONCE(node_->get_logger(), "::Watchdog exception: " << e.what());
+                    RCLCPP_INFO_STREAM_ONCE(node_->get_logger(), "::The shutdown signal was received!");
+                    shutdown_signaler_->shutdown();
+                }
 
             }
 
